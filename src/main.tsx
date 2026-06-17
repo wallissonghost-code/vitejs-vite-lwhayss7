aa11f1c33e3c61a616703ff00791700006c4a898
wallissonghost-code <wallissonghost@gmail.com>
2026-06-17 20:34:13 -0300

    Mount floating admin panel

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { AdminFloatingPanel } from "./AdminFloatingPanel";
 import "./styles.css";
 import "./backend.css";
+import "./admin.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
+    <AdminFloatingPanel />
   </React.StrictMode>
 );
