32b06d8012cad4a6646acd43fe69cade61937bbb
wallissonghost-code <wallissonghost@gmail.com>
2026-06-17 20:12:59 -0300

    Proxy API and uploads to Express server

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()]
-  });
+  plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+    proxy: {
+      "/api": "http://localhost:3001",
+      "/uploads": "http://localhost:3001"
+    }
+  }
+});
