d4b0b7aab653a647cd8cb8519f2f69460f50e903
wallissonghost-code <wallissonghost@gmail.com>
2026-06-17 20:28:33 -0300

    Add wallet route placeholder

--- server/profileRoutes.js
+++ server/profileRoutes.js
@@ -5,4 +5,8 @@ export function registerProfileRoutes(app) {
   app.get("/api/profile", requireAuth, (req, res) => {
     res.json(publicUser(req.user));
   });
+
+  app.post("/api/wallet/recharge", requireAuth, (req, res) => {
+    res.json(publicUser(req.user));
+  });
 }
