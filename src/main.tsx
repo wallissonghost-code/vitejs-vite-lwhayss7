4826b1ec613efbb7b41d7a4f11fc6ac2cbccc6cb
wallissonghost-code <wallissonghost@gmail.com>
2026-06-17 20:40:55 -0300

    Mount real notifications panel

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AdminFloatingPanel } from "./AdminFloatingPanel";
+import { NotificationsPanel } from "./NotificationsPanel";
 import { PublicProfileRouter } from "./PublicProfile";
 import { PublicProfileShortcut } from "./PublicProfileShortcut";
 import "./styles.css";
 import "./backend.css";
 import "./admin.css";
 import "./public-profile.css";
+import "./notifications.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
     <AdminFloatingPanel />
     <PublicProfileShortcut />
+    <NotificationsPanel />
     <PublicProfileRouter />
   </React.StrictMode>
 );
