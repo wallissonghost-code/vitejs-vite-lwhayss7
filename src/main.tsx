9585b3871714b558bd144733e713c6f3e21695a5
wallissonghost-code <wallissonghost@gmail.com>
2026-06-17 20:43:53 -0300

    Mount algorithmic feed panel

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AdminFloatingPanel } from "./AdminFloatingPanel";
+import { AlgorithmFeed } from "./AlgorithmFeed";
 import { NotificationsPanel } from "./NotificationsPanel";
 import { PublicProfileRouter } from "./PublicProfile";
 import { PublicProfileShortcut } from "./PublicProfileShortcut";
@@ -10,6 +11,7 @@ import "./backend.css";
 import "./admin.css";
 import "./public-profile.css";
 import "./notifications.css";
+import "./algorithm.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -17,6 +19,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <AdminFloatingPanel />
     <PublicProfileShortcut />
     <NotificationsPanel />
+    <AlgorithmFeed />
     <PublicProfileRouter />
   </React.StrictMode>
 );
