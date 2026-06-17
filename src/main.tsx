a1725d900572976dd236dfa18b906b1c5c4730cf
wallissonghost-code <wallissonghost@gmail.com>
2026-06-17 20:37:14 -0300

    Mount public profile router

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AdminFloatingPanel } from "./AdminFloatingPanel";
+import { PublicProfileRouter } from "./PublicProfile";
+import { PublicProfileShortcut } from "./PublicProfileShortcut";
 import "./styles.css";
 import "./backend.css";
 import "./admin.css";
+import "./public-profile.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
     <AdminFloatingPanel />
+    <PublicProfileShortcut />
+    <PublicProfileRouter />
   </React.StrictMode>
 );
